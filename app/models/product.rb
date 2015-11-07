246b1424c7c43dbfd22715c74d85637de4665d2a
jwilsontt <jwilsontt@gmail.com>
2015-11-07 17:11:09 -0400

    Added a cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url,
 	presence: true
 
@@ -14,5 +18,18 @@ class Product < ActiveRecord::Base
 	
 	def self.latest
 		Product.order(:updated_at).last
+	end
+
+	private
+
+	#ensure that there are no line items referencing this product
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
 	end
 end
