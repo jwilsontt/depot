9c18200a0af535ad78332b4dbbfb6883761c42e8
jwilsontt <jwilsontt@gmail.com>
2015-11-17 11:53:55 -0500

    Added email config

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   resources :products
 
