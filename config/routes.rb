3cdaaa4d47eb95c755abcedbebe28016de70f75d
jwilsontt <jwilsontt@gmail.com>
2015-11-27 01:30:18 -0400

    Added i18n

--- config/routes.rb
+++ config/routes.rb
@@ -14,17 +14,23 @@ Rails.application.routes.draw do
   
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
+  # resources :orders
+  # resources :line_items
+  # resources :carts
   get 'store/index'
   resources :products do
     get :who_bought, on: :member
   end
 
   resources :products
+  
+  scope '(:locale)' do
+   resources :orders
+   resources :line_items
+   resources :carts
+   root 'store#index', as: 'store', via: :all 
+  end
 
-  root 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
