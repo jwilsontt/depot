9072ec286c7caf63b74e189985ba54088a3f5275
jwilsontt <jwilsontt@gmail.com>
2015-12-05 13:56:59 -0400

    Updated config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,43 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-server '172.16.154.132', port: 80, roles: [:web, :app, :db], primary: true
+# Change these
+server '172.16.154.132', port: your_port_num, roles: [:web, :app, :db], primary: true
 
-set :application, 'depot'
-set :repo_url, 'git@github.com:jwilsontt/depot.git'
-set :user, 'deploy'
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :repo_url,        'git@github.com:jwilsontt/depot.git'
+set :application,     'depot'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -54,6 +25,17 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
